613373c4dc11ba5875fe71ba56aaa500db04290b
Moumni Hamza <94747@supinfo.com>
2015-07-06 01:00:17 -0400

    ADDED a fulltitle helper if the title is present it returne the page_title + base_title  , if there is no title specified it will return the base_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+def fulltitle(page_title = "")
+	base_title = "Welcome to Social Network"
+	if page_title.empty?
+		base_title
+	else
+		page_title + '|' + base_title
+	end
+end
 end
