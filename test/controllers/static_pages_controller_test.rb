53d5284bc691c3b73f0a72b18416bbe119a8d0a8
Moumni Hamza <94747@supinfo.com>
2015-07-05 23:38:10 -0400

    Made automated test to add about menu and titles to Home Help and About

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "home", "Home | Welcome to SocialNetwork"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "help", " Help | Welcome to SocialNetwork"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "About", " About | Welcome to SocialNetwork"
   end
 
 end
