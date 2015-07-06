31c850182685027d7120c72886421897d1c7f3e3
Moumni Hamza <94747@supinfo.com>
2015-07-06 00:40:52 -0400

    Finished the static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,13 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "home", "Home | Welcome to SocialNetwork"
+    assert_select "Home", "Home | Welcome to SocialNetwork"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "help", " Help | Welcome to SocialNetwork"
+    assert_select "Help", " Help | Welcome to SocialNetwork"
   end
 
   test "should get about" do
